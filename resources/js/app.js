011e755ab812648671e24dfbaa524f434e140051
CristianLeyton <leytoncristian96@gmail.com>
2025-09-08 22:55:40 -0300

    Vue instalado

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App.vue'; // Assuming you'll create App.vue
+
+createApp(App).mount('#app');
---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+    <!-- resources/js/components/App.vue -->
+    <template>
+        <div>
+            <h1 class="text-sm font-black text-amber-700">Hello from Vue! Mamá!</h1>
+        </div>
+    </template>
+
+    <script>
+    export default {
+        // Component logic
+    }
+    </script>
